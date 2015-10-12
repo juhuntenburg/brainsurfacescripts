96440c37ef8ffe226403fdb24219dbd13ade9543
Julia Huntenburg <ju.huntenburg@gmail.com>
2015-10-12 11:02:09 +0200

    fixing vtk read so it can handle python3 division

--- vtk_rw.py
+++ vtk_rw.py
@@ -10,24 +10,26 @@ TO DO: add reading comments
 '''
 
 def read_vtk(file):
+    import pandas as pd
+    import numpy as np
     # read full file while dropping empty lines 
-    vtk_df=pd.read_csv(file, header=None)
+    vtk_df=pd.read_csv(file, header=None, engine='python')
     vtk_df=vtk_df.dropna()
     # extract number of vertices and faces
     number_vertices=int(vtk_df[vtk_df[0].str.contains('POINTS')][0].iloc[0].split()[1])
     number_faces=int(vtk_df[vtk_df[0].str.contains('POLYGONS')][0].iloc[0].split()[1])
     # read vertices into df and array
     start_vertices= (vtk_df[vtk_df[0].str.contains('POINTS')].index.tolist()[0])+1
-    vertex_df=pd.read_csv(file, skiprows=range(start_vertices), nrows=number_vertices, sep='\s*', header=None)
+    vertex_df=pd.read_csv(file, skiprows=range(start_vertices), nrows=number_vertices, sep='\s*', header=None, engine='python')
     if np.array(vertex_df).shape[1]==3:
         vertex_array=np.array(vertex_df)
     # sometimes the vtk format is weird with 9 indices per line, then it has to be reshaped
     elif np.array(vertex_df).shape[1]==9:
-        vertex_df=pd.read_csv(file, skiprows=range(start_vertices), nrows=number_vertices/3+1, sep='\s*', header=None)
+        vertex_df=pd.read_csv(file, skiprows=range(start_vertices), nrows=int(number_vertices/3)+1, sep='\s*', header=None, engine='python')
         vertex_array=np.array(vertex_df.iloc[0:1,0:3])
         vertex_array=np.append(vertex_array, vertex_df.iloc[0:1,3:6], axis=0)
         vertex_array=np.append(vertex_array, vertex_df.iloc[0:1,6:9], axis=0)
-        for row in range(1,(number_vertices/3+1)):
+        for row in range(1,(int(number_vertices/3)+1)):
             for col in [0,3,6]:
                 vertex_array=np.append(vertex_array, array(vertex_df.iloc[row:(row+1),col:(col+3)]),axis=0) 
         # strip rows containing nans
@@ -36,26 +38,29 @@ def read_vtk(file):
         print "vertex indices out of shape"
     # read faces into df and array
     start_faces= (vtk_df[vtk_df[0].str.contains('POLYGONS')].index.tolist()[0])+1
-    face_df=pd.read_csv(file, skiprows=range(start_faces), nrows=number_faces, sep='\s*', header=None)
+    face_df=pd.read_csv(file, skiprows=range(start_faces), nrows=number_faces, sep='\s*', header=None, engine='python')
     face_array=np.array(face_df.iloc[:,1:4])
     # read data into df and array if exists
     if vtk_df[vtk_df[0].str.contains('POINT_DATA')].index.tolist()!=[]:
         start_data=(vtk_df[vtk_df[0].str.contains('POINT_DATA')].index.tolist()[0])+3
         number_data = number_vertices
-        data_df=pd.read_csv(file, skiprows=range(start_data), nrows=number_data, sep='\s*', header=None)
+        data_df=pd.read_csv(file, skiprows=range(start_data), nrows=number_data, sep='\s*', header=None, engine='python')
         data_array=np.array(data_df)
     else:
-        data = np.empty(0)
+        data_array = np.empty(0)
     
     return vertex_array, face_array, data_array
 
 
 
 def write_vtk(filename, vertices, faces, data=None, comment=None):
+    import pandas as pd
+    import numpy as np
     # infer number of vertices and faces
     number_vertices=vertices.shape[0]
     number_faces=faces.shape[0]
-    number_data=data.shape[0]
+    if data is not None:
+        number_data=data.shape[0]
     # make header and subheader dataframe
     header=['# vtk DataFile Version 3.0',
             '%s'%comment,
